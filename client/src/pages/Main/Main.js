9f409178224a0e8328e7477b995499083af91693
My Lord <page.c.tyler@gmail.com>
2021-02-17 00:34:29 -0500

    able to fetch csv from s3. not all must be an access setting.

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from '../../components/Grid';
 import DataList from '../../components/DataList'; 
+import API from '../../utils/API';
 
 
 export default function Main() {
 
     const [ infectionRates, setInfectionRates ] = useState('Infection Rates by Region since 01/01/2020'),
     [ tweetsTrending, setTweetsTrending ] = useState('Rolling count of Most Used Twitter Hashtags'),
-    [ toneGauge, setToneGauge ] = useState('Gauge depicting tone of tweets since 12/11/2020')
+    [ toneGauge, setToneGauge ] = useState('Gauge depicting tone of tweets since 12/11/2020'),
+    [ wordCount1211to1225, setCount1211to1225 ] = useState()
+
+    useEffect(() => {
+        getTwitterCount()
+    },[])
+    const getTwitterCount = async() => {
+        const wordCount = await API.getTweetWordCount()
+        setCount1211to1225(wordCount)
+        console.log(wordCount)
+    }
 
     return (
         <Container>
